ef255ec3e6b838a7dec7bc6c2212f7dda850ea91
Gabriel Lacunza <gab.lacunza@gmail.com>
2022-06-08 09:14:21 -0400

    base de datos hecha

--- TiendaMascotas/main/models.py
+++ TiendaMascotas/main/models.py
@@ -2,14 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-"""
+
 #modelo de categoria
 class Categoria(models.Model):
-    idCategoria = models.IntegerField(primary_key=True, verbose_name="id de la categoria")
+    idCategoria = models.IntegerField(primary_key=True)
     nombreCategoria = models.CharField(max_length=80, blank=False, null=False)
     
     def __str__(self) -> str:
         return self.nombreCategoria
 
-#class Vehiculo(models.Model):
-"""
+#modelo de producto
+class Producto(models.Model):
+    idProduco = models.IntegerField(primary_key=True)
+    categoria = models.ForeignKey(Categoria, models.CASCADE, blank=False, null=False)
+    nombreProducto = models.CharField(max_length=100, blank=False, null=False)
+    precioProducto = models.IntegerField(blank=False, null=False)
+    descripcionProducto = models.CharField(max_length=300, blank=False, null=False)
+    disponibilidadProducto = models.BooleanField(blank=False, null=False)
