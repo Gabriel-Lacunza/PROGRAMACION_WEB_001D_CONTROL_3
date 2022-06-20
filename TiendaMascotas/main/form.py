8e14cbde13c9feb88218cce1eb9cb0dfd1c897cb
Gabriel Lacunza <gab.lacunza@gmail.com>
2022-06-20 13:04:11 -0400

    actualizado

--- TiendaMascotas/main/form.py
+++ TiendaMascotas/main/form.py
@@ -1,10 +1,12 @@
 from django import forms
+from django.forms import ModelForm
+from .models import Producto
 
-class log(forms.Form):
+class formulario_ingresar(forms.Form):
     uName = forms.CharField(max_length=50)
     passw = forms.CharField(max_length=50)
 
-class registrar(forms.Form):
+class formuario_registrar(forms.Form):
     rut = forms.CharField(max_length=10)
     nombres = forms.CharField(max_length=50)
     apellidos = forms.CharField(max_length=50)
@@ -15,18 +17,10 @@ class registrar(forms.Form):
     contraseñaRepetida = forms.CharField(max_length=50)
     imagen = forms.ImageField()
 
-class mantenerdorProducto(forms.Form):
-    choices = [("1", "perro"), ("2", "gato")]
-    idProducto = forms.CharField(max_length=100)
-    categoriaProducto = forms.ChoiceField(choices=choices)
-    nombreProducto = forms.CharField(max_length=100)
-    descripcionProducto = forms.CharField(max_length=500)
-    precioProducto = forms.IntegerField()
-    descuentoSuscriptor = forms.IntegerField(max_value=100)
-    descuentoOferta = forms.IntegerField(max_value=100)
-    imgenProducto = forms.FileField()
-    cantidadProducto = forms.IntegerField()
-    imagenProducto = forms.ImageField()
+class mantenerdorProducto(ModelForm):
+    class Meta:
+        model = Producto
+        fields = ["imagenProducto", "imagenProducto", "categoria", "nombreProducto", "precioProducto", "descripcionProducto", "disponibilidadProducto"]
 
 class mantenedorUsuario(forms.Form):
     choices = [("1", "cliente"), ("2", "admin")]
@@ -34,10 +28,14 @@ class mantenedorUsuario(forms.Form):
     tipoUsuario = forms.ChoiceField(choices=choices, widget=forms.RadioSelect)
     rutUsusario = forms.CharField(max_length=10)
     nombreUsuario = forms.CharField(max_length=50)
-    direccionUsuario = forms.CharField(max_length=500)
+    apellidoUsuario = forms.CharField(max_length=50)
+    direccionUsuario = forms.CharField(max_length=500, widget=forms.Textarea)
     SuscripcionUsuario = forms.BooleanField()
     contrasenaUsuario = forms.CharField(max_length=50, min_length=10)
     imagenUsuario = forms.FileField()
 
-class pedidos(forms.Form):
-    cantidad = forms.IntegerField()
+class mantenedorBodega(forms.Form):
+    choiceCategoria = [("1", "perro"), ("2", "gato")]
+    idBodega = forms.IntegerField()
+    categoriaBodega = forms.ChoiceField()
+    cantidadBodega = forms.IntegerField()
--- TiendaMascotas/main/views.py
+++ TiendaMascotas/main/views.py
@@ -6,131 +6,115 @@ from .models import Producto, Usuario
 listaProductos = []
 totalPedido = 0
 
-def adminTienda(request):
-    return render(request, 'administrar_tienda.html', {})
-
-def carritoCompra(request):
+def carrito_de_compras(request): 
+    """
+    falta que la pagina calcule el total por producto
+    """
     lista = listaProductos
     t = totalPedido
     return render(request, 'carrito_de_compras.html', {"lista": lista, "total": t})
 
-def detalleFactura(request):
+def detalle_factura(request):
     lista = listaProductos
     """
     imprimir lo que listaProductos tenga
     """
     return render(request, 'detalle_factura.html', {"lista": lista})
 
-def fichaProducto(request, idProduco):
+def ficha_producto(request, idProduco): #listo
     p = Producto.objects.get(idProduco = idProduco)
-    formu = pedidos
-    total = totalPedido
+    return render(request, 'ficha_producto.html', {"p": p})
 
-    if request.method == "POST":
-        formu = pedidos(request.POST)
-        if formu.is_valid():
-            c = formu.cleaned_data["cantidad"]
-            listaProductos.append([p.categoria.nombreCategoria, p.nombreProducto, p.precioProducto*c])
-            total += p.precioProducto*c
-            print(listaProductos)
-        else:
-            formu = pedidos
-    return render(request, 'ficha_producto_anon.html', {"p": p, "form": formu})
+def historial_de_ventas(request):
+    """
+    ver como hacer la pagina
+    """
+    return render(request, 'historial_de_ventas.html', {})
 
-def index(request):
+def ingresar(request): #arreglar formulario en html
+    form = formulario_ingresar
+
+    if request.method == "post":
+        if form.is_valid:
+            return redirect(to=inicio_como_cliente)
+    """
+    que se valide la llegada de un usuario
+    confirmar que te redirija a la pagina inicio cli
+    """
+    return render(request, 'ingresar.html', {"form": form})
+
+def inicio_como_administrador(request):
+    p = Producto.objects.all()
+    """
+    que el boton "editar" te lleve a su maestroProducto, con los datos ya cargados
+    """
+    return render(request, 'inicio_como_administrador.html', {"p": p})
+
+def inicio_como_cliente(request): #listo
     prd = Producto.objects.all()
-    return render(request, 'index.html', {"prd": prd})
+    return render(request, 'inicio_como_cliente.html', {"prd": prd})
 
-def inicioCli(request):
+def inicio_usuario_anonimo(request): #listo
     prd = Producto.objects.all()
-    return render(request, 'inicio_cliente.html', {"prd": prd})
+    return render(request, 'inicio_usuario_anonimo.html', {"prd": prd})
 
-def inventario(request):
+def mantenedor_de_bodega(request): #confirmar si el formulario en html funciona
+    """
+    hacer que rellene la base de datos
+    """
     p = Producto.objects.all()
 
     if request.method == "POST":
-        form = mantenerdorProducto(request.POST)
+        form = mantenedorBodega(request.POST)
+        
         if form.is_valid():
             i = form.cleaned_data["idProducto"]
             c = form.cleaned_data["categoriaProducto"]
             n = form.cleaned_data["nombreProducto"]
             can = form.cleaned_data["cantidadProducto"]
             im = form.cleaned_data["imagenProducto"]
-            pr = Producto(imagenProducto = im, idProduco = i, categoria = c,  nombreProducto = n, cantidadProducto = can)
-            pr.save
-        else:
-            form = mantenerdorProducto
-    return render(request, 'inventario.html', {"form": form, "p": p})
+            d = form.cleaned_data["disponibilidad"]
+            pr = Producto(imagenProducto = im, idProduco = i, categoria = c,  nombreProducto = n, cantidadProducto = can, disponibilidadProducto = d)
+            pr.save()
+    else:
+        form = mantenedorBodega()
+    return render(request, 'mantenedor_de_bodega.html', {"form": form, "p": p})
 
-def login(request):
-    form = log
-
-    if request.method == "post":
-        if form.is_valid:
-            return redirect(to=inicioCli)
-    """
-    que se valide la llegada de un usuario
-    """
-    return render(request, 'login.html', {"form": form})
-
-def maestroProducto(request):
-    form = mantenerdorProducto
+def mantenedor_de_productos(request): #confirmar si el formulario en html funciona
     p = Producto.objects.all()
 
-    if request.method == "POST":
-        form = mantenerdorProducto(request.POST)
-        if form.is_valid():
-            i = form.cleaned_data["idProducto"]
-            c = form.cleaned_data["categoriaProducto"]
-            n = form.cleaned_data["nombreProducto"]
-            d = form.cleaned_data["descripcionProducto"]
-            pre = form.cleaned_data["precioProducto"]
-            desS = form.cleaned_data["descuentoSuscriptor"]
-            desO = form.cleaned_data["descuentoOferta"]
-            im = form.cleaned_data["imagenProducto"]
-            pr = Producto(imagenProducto = im, idProduco = i, categoria = c,  nombreProducto = n, descripcionProducto = d, precioProducto = pre, descuentoSuscriptor = desS, descuentoOferta = desO)
-            pr.save
-        else:
-            form = mantenerdorProducto
-    """
-    pendiente
-    que se agreguen / actualicen los datos
-    """
-    return render(request, 'maestro_producto.html', {"form": form, "p": p})
+    if request.method == "POST": #listo
+        form = mantenerdorProducto(request.POST, request.FILES)
+        print(request.FILES)
+        if form.is_valid:
+            form.save()
+    else:
+        form = mantenerdorProducto()
+    return render(request, 'mantenedor_de_productos.html', {"form": form, "p": p})
 
-def maestroUsuario(request):
+def mantenedor_de_usuarios(request): #confirmar si el formulario en html funciona
     form = mantenedorUsuario
     u = Usuario.objects.all()
     """
-    que los datos se agreguen / actualizen
+    hacer que los datos se agreguen / actualizen
     """
-    return render(request, 'maestro_usuario.html', {"form": form, "u": u})
+    return render(request, 'mantenedor_de_usuarios.html', {"form": form, "u": u})
 
-def mainAdmin(request):
-    p = Producto.objects.all()
-    """
-    que el boton "editar" te lleve a su maestroProducto, con los datos ya cargados
-    """
-    return render(request, 'main_administrador.html', {"p": p})
+def menu_de_administracion(request): #listo
+    return render(request, 'menu_de_administracion.html', {})
 
-def misCompras(request):
+def mis_compras(request):
     """
     ver como hacer le registro de compras
     """
     return render(request, 'mis_compras.html', {})
 
-def registroVentas(request):
-    """
-    ver como hacer la pagina
-    """
-    return render(request, 'registro_ventas.html', {})
+def nosotros(request): #listo
+    return render(request, 'nosotros.html', {})
 
-def registro(request):
-    form = registrar
+def registrarse(request): #arreglar formulario en html
+    form = formuario_registrar
     """
     que se guarde la informacion
     """
-    return render(request, 'registro.html', {"form": form})
-
-def sobreNos(request):
-    return render(request, 'sobre_nosotros.html', {})
+    return render(request, 'registrarse.html', {"form": form})
