9f160386723b618ae2315efbdb2113fe032b27ea
Diego Gaviria <di.gaviria@duocuc.cl>
2022-07-07 12:02:39 -0400

    API terminada

--- TiendaMascota/rest_tienda/urls.py
+++ TiendaMascota/rest_tienda/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from rest_tienda.views import *
 
 urlpatterns = [
-    path('producto_read_all', producto_read_all , name="producto_read_all"),
-    path('producto_create/', producto_create.as_view(), name="producto_create"),
+    path('lista_productos', lista_productos , name="lista_productos"),
+    path('detalle_producto/<id>', detalle_producto , name="detalle_producto"),
+    path('producto_create', producto_create.as_view(), name="producto_create"),
+    path('vehiculo_delete/<id>', vehiculo_delete, name="vehiculo_delete"),
+    path('producto_update', producto_update.as_view(), name="producto_update"),
     path('login', login, name='login'),
 ]
+
--- TiendaMascota/rest_tienda/views.py
+++ TiendaMascota/rest_tienda/views.py
@@ -29,19 +29,48 @@ def lista_productos(request):
         else:
             return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'PUT', 'DELETE'])
+def detalle_producto(request, id):
+    try:
+        producto= Producto.objects.get(idProducto=id)
+    except Producto.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer= ProductoSerializer(producto)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ProductoSerializer(producto, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        producto.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
 def producto_read_all(request):
     if request.method == 'GET':
         list = Producto.objects.all()
         serializer = ProductoSerializer(list, many=True)
         return Response(serializer.data)
-        
 
+#@permission_classes((IsAuthenticated,))
 class producto_create(APIView):
     def post(self, request, format=None):
         serializer = ProductoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class producto_update(APIView):
+    def put(self, request, format=None):
+        serializer = ProductoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.update()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -60,3 +89,13 @@ def login(request):
     token, created = Token.objects.get_or_create(user=user)
     print(f"Este es el token creado: '{token.key}'")
     return Response(token.key)
+
+@csrf_exempt
+@api_view(['DELETE'])
+def vehiculo_delete(request, id):
+    if request.method == 'DELETE':
+        try:
+            Producto.objects.get(idProducto=id).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Producto.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
